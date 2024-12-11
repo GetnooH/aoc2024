df3de3dea1eddf56583a43c0684bc1b0eedac95c
Gaétan <gaetan@getnooh.io>
2024-12-11 01:15:06 +0100

    Day 3 - Part 2

--- src/day03/index.ts
+++ src/day03/index.ts
@@ -17,8 +17,27 @@ const part1 = (rawInput: string) => {
 
 const part2 = (rawInput: string) => {
     const input = parseInput(rawInput)
-
-    return
+    const regex = new RegExp('don\'t\\(\\)|do\\(\\)|mul\\((?<A>[0-9]{1,3}),(?<B>[0-9]{1,3})\\)', "g")
+    const matches = input.matchAll(regex)
+    let sum = 0
+    let toggle = true
+    for (const match of matches) {
+        if (match[0] === 'don\'t()') {
+            toggle = false
+            continue
+        }
+        if (match[0] === 'do()') {
+            toggle = true
+            continue
+        }
+        if (match[0].startsWith('mul')) {
+            if (!toggle) continue
+            let a = Number.parseInt(match?.groups?.A ?? "0");
+            let b = Number.parseInt(match?.groups?.B ?? "0");
+            sum += a * b;
+        }
+    }
+    return sum.toString()
 }
 
 run({
@@ -33,10 +52,10 @@ run({
     },
     part2: {
         tests: [
-            // {
-            //   input: ``,
-            //   expected: "",
-            // },
+            {
+                input: `xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))`,
+                expected: "48",
+            },
         ],
         solution: part2,
     },
